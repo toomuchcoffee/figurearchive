eee87c05563af6521deb61afbfd7a8740a4ca679
toomuchcoffee <2muchcoffee@gmx.de>
2019-03-20 00:37:33 +0100

    adds product lines

--- src/main/java/de/toomuchcoffee/figurearchive/entitiy/ProductLine.java
+++ src/main/java/de/toomuchcoffee/figurearchive/entitiy/ProductLine.java
@@ -7,9 +7,41 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ProductLine {
+    // inspired by JediBusiness.com
     KENNER("Vintage Kenner"),
     POTF2("Power of the Force 2"),
-    POTJ("Power of the Jedi");
+    SOTE("Shadows of the Empire"),
+    EPISODE1("Episode 1"),
+    POTJ("Power of the Jedi"),
+    SAGA("Saga"),
+    DC("Star Tours"),
+    OCW("Clone Wars"),
+    CWANIMATED("Clone Wars Animated Style"),
+    OTC("Original Trilogy Collection"),
+    ROTS("Revenge of the Sith"),
+    TSC("The Saga Collection"),
+    TAC("The Anniversary Collection"),
+    TLC("The Legacy Collection"),
+    TCW("The Clone Wars"),
+    SOTDS("Shadow of the Dark Side"),
+    TVC("The Vintage Collection"),
+    DTF("Discover the Force"),
+    MH("Movie Heroes"),
+    TLC13("The Legacy Collection 2013"),
+    SL("Saga Legends"),
+    TBS("The Black Series"),
+    TBS6("The Black Series 6 inch"),
+    SWR("Star Wars Rebels Saga Legends"),
+    TFA("The Force Awakens"),
+    RO("Rogue One"),
+    TLJ("The Last Jedi"),
+    SOLO("Solo: A Star Wars Story"),
+    GOA("Galaxy of Adventures"),
+    RESISTANCE("Star Wars Resistance"),
+    DISNEY("All Disney Lines"),
+    SHFIGUARTS("SH Figuarts"),
+    BANDAI("Bandai Model Kit"),
+    HERO("12\" Hero Series");
 
     private final String description;
 }
