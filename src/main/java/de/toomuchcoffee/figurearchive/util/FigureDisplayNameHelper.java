9ce215d055520700e1624980452c8617e6dc2764
toomuchcoffee <2muchcoffee@gmx.de>
2019-11-21 01:07:49 +0100

    adds placement no to display name of figure

--- src/main/java/de/toomuchcoffee/figurearchive/util/FigureDisplayNameHelper.java
+++ src/main/java/de/toomuchcoffee/figurearchive/util/FigureDisplayNameHelper.java
@@ -1,13 +1,23 @@
 package de.toomuchcoffee.figurearchive.util;
 
 import de.toomuchcoffee.figurearchive.entity.Figure;
+import de.toomuchcoffee.figurearchive.entity.ProductLine;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.List;
 import java.util.Optional;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 public class FigureDisplayNameHelper {
     public static String getDisplayName(Figure figure) {
-        return Optional.ofNullable(figure.getProductLine())
-                .map(l -> String.format("%s (%s)", figure.getVerbatim(), l.name()))
-                .orElse(figure.getVerbatim());
+        List<String> parts = newArrayList(figure.getVerbatim());
+        Optional.ofNullable(figure.getProductLine())
+                .map(ProductLine::name)
+                .ifPresent(parts::add);
+        Optional.ofNullable(figure.getPlacementNo())
+                .filter(StringUtils::isNotBlank)
+                .ifPresent(parts::add);
+        return String.join(", ", parts);
     }
 }
