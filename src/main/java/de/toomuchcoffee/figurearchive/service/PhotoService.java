2fc28fc5b9059925b26269f73d45159910dd226c
toomuchcoffee <2muchcoffee@gmx.de>
2019-11-29 03:54:05 +0100

    adds transactional

--- src/main/java/de/toomuchcoffee/figurearchive/service/PhotoService.java
+++ src/main/java/de/toomuchcoffee/figurearchive/service/PhotoService.java
@@ -102,6 +102,7 @@ public class PhotoService {
     }
 
     @LogExecutionTime
+    @Transactional
     public void save(Photo photo, OwningSideOfRelation<Figure, Photo> owningSideOfRelation) {
         photoRepository.save(photo);
         figureRepository.saveAll(owningSideOfRelation.collectChangedOwners());
