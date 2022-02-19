d26499e84c7a0be8903d894fd2d8244362a67162
toomuchcoffee <2muchcoffee@gmx.de>
2022-02-19 18:25:33 +0100

    fixes

--- src/main/java/de/toomuchcoffee/figurearchive/repository/FigureRepository.java
+++ src/main/java/de/toomuchcoffee/figurearchive/repository/FigureRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FigureRepository extends JpaRepository<Figure, Long> {
     Page<Figure> findByProductLine(String productLine, Pageable pageable);
 
-    Page<Figure> findByProductLine(String productLine);
+    List<Figure> findByProductLine(String productLine);
 
     long countByProductLine(String productLine);
 }
